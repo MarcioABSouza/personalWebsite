7aa62ce932a8e482e9619c312fd7b09800bd0593
Marcio Souza <marcio.souza@servexternos.gruposantander.com>
2022-02-05 15:25:04 -0300

    Refactoring Descriptions and Texts

--- script.js
+++ script.js
@@ -1,137 +1,123 @@
-const menu = document.querySelector("#menu")
-const nav = document.querySelector("#nav")
-const exit = document.querySelector("#exit")
-const scroll = document.querySelector('.scroll')
-const preloader = document.querySelector('#preloader')
-const phone = document.querySelector('#Phone')
-const phoneText = document.querySelector('#Text')
-const whoami = document.querySelector('.whoami')
+const menu = document.querySelector("#menu");
+const nav = document.querySelector("#nav");
+const exit = document.querySelector("#exit");
+const scroll = document.querySelector(".scroll");
+const preloader = document.querySelector("#preloader");
+const phone = document.querySelector("#Phone");
+const phoneText = document.querySelector("#Text");
+const whoami = document.querySelector(".whoami");
 const toTop = document.getElementById("myBtn");
 
 //PRELOADER
 
-window.addEventListener('load', ()=>{
-    setTimeout(()=>{
-        preloader.style.opacity = 0;
-       
-        typed2.pause.status = true;
-    },2000)
+window.addEventListener("load", () => {
+  setTimeout(() => {
+    preloader.style.opacity = 0;
 
-    setTimeout(()=>{
-        preloader.style.display = 'none';
-    },3000)
-    
-    
-})
+    typed2.pause.status = true;
+  }, 2000);
+
+  setTimeout(() => {
+    preloader.style.display = "none";
+  }, 3000);
+});
 
 //NAVBAR
 
-let menuItens = document.querySelectorAll('#nav a ');
+let menuItens = document.querySelectorAll("#nav a ");
 
-scroll.addEventListener('click', () =>{
-    
-    window.scroll({
-        top: 690,
-        behavior: 'smooth'
-    })
-    
-})
+scroll.addEventListener("click", () => {
+  window.scroll({
+    top: 690,
+    behavior: "smooth",
+  });
+});
 
-menu.addEventListener('click', function(e) {
-    nav.classList.toggle('hide-mobile')
-    e.preventDefault()    
-})
+menu.addEventListener("click", function (e) {
+  nav.classList.toggle("hide-mobile");
+  e.preventDefault();
+});
 
-exit.addEventListener('click', (e)=>{
-    nav.classList.toggle('hide-mobile')
-    e.preventDefault()
-} )
-
+exit.addEventListener("click", (e) => {
+  nav.classList.toggle("hide-mobile");
+  e.preventDefault();
+});
 
 //PHONE ANIMATION
 
-whoami.addEventListener('mouseover', ()=>{
-    phone.classList.add('animateLeftRight')
-    setTimeout(()=>{phoneText.classList.add('animateLeftRight')},500)
-    
-})
+whoami.addEventListener("mouseover", () => {
+  phone.classList.add("animateLeftRight");
+  setTimeout(() => {
+    phoneText.classList.add("animateLeftRight");
+  }, 500);
+});
 
 //SCROLL
 
-function scrollToId(event){
+function scrollToId(event) {
+  event.preventDefault();
+  const element = event.target;
+  const id = element.getAttribute("href");
+  const to = document.querySelector(id).offsetTop;
 
-    event.preventDefault()
-    const element = event.target;
-    const id = element.getAttribute('href')
-    const to = document.querySelector(id).offsetTop;
-    
+  window.scroll({
+    top: to,
+    behavior: "smooth",
+  });
 
-   
-
-    window.scroll({
-        top: to, 
-        behavior: 'smooth'
-    })
-
-    nav.classList.toggle('hide-mobile')
+  nav.classList.toggle("hide-mobile");
 }
 
-menuItens.forEach((item)=>{
-    item.addEventListener('click', scrollToId)
-})
-
+menuItens.forEach((item) => {
+  item.addEventListener("click", scrollToId);
+});
 
 //TYPED
 
-var typed = new Typed('#forTyping', {
-    strings: [
-        'semi-nerd. ',
-        'fã do Torvalds.',
-        'entusiasta da programação.'],
-    typeSpeed: 50,
-    backSpeed: 50,
-    smartBackspace: true, // this is a default
-    loop: true, 
-    showCursor: false,
+var typed = new Typed("#forTyping", {
+  strings: ["programador. ", "fã do Torvalds.", "minimalista.", "amante do software livre."],
+  typeSpeed: 50,
+  backSpeed: 50,
+  smartBackspace: true, // this is a default
+  loop: true,
+  showCursor: false,
+});
 
-  });
+var typed2 = new Typed("#forTypingPreloader", {
+  strings: [".", "..", "..."],
+  typeSpeed: 10,
+  backSpeed: 10,
+  loop: true,
+  showCursor: false,
+});
 
-var typed2 = new Typed('#forTypingPreloader', {
-    strings: ['.','..','...'],
-    typeSpeed: 10,
-    backSpeed: 10,
-    loop: true ,
-    showCursor: false,
+var typed3 = new Typed("#findme", {
+  strings: [
+    ' facilmente me chamar no <span class="main-color">Facebook</span>.',
+    ' ver os códigos dos projetos no <span class="main-color">Github</span>.',
+    ' me enviar uma mensagem no  <span class="main-color">Whatsapp</span>.',
+    ' dar uma olhada no meu perfil no  <span class="main-color">Linkedin</span>.',
+    ' ou me enviar um email em <span class="main-color">marciosouzaprofissional@gmail.com</span>',
+  ],
 
-  });
-
-
-  var typed3 = new Typed('#findme', {
-    strings: [
-        
-              ' facilmente me chamar no <span class="main-color">Facebook</span>.',
-              ' ver os códigos dos projetos no <span class="main-color">Github</span>.',
-              ' me enviar uma mensagem no  <span class="main-color">Whatsapp</span>.',
-              ' dar uma olhada no meu perfil no  <span class="main-color">Linkedin</span>.',
-              ' ou me enviar um email em <span class="main-color">marciosouzaprofissional@gmail.com</span>'],
-
-    typeSpeed: 10,
-    backSpeed: 10,
-    backDelay: 1500,
-    loop: true ,
-    showCursor: false,
-    
-
-  });
+  typeSpeed: 10,
+  backSpeed: 10,
+  backDelay: 1500,
+  loop: true,
+  showCursor: false,
+});
 
 //SCROLL TO TOP
 
-
-
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function () {
+  scrollFunction();
+};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 400 || document.documentElement.scrollTop > 400) {
+  if (
+    document.body.scrollTop > 400 ||
+    document.documentElement.scrollTop > 400
+  ) {
     toTop.style.opacity = 1;
   } else {
     toTop.style.opacity = 0;
@@ -139,8 +125,8 @@ function scrollFunction() {
 }
 
 function topFunction() {
-    window.scroll({
-        top: 0,
-        behavior: 'smooth'
-    })
+  window.scroll({
+    top: 0,
+    behavior: "smooth",
+  });
 }
